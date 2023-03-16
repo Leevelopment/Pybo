f2287de66f515d6b30c7534951f4a04c26d30194
Leevelopment <jaemyung123@naver.com>
2023-03-16 16:30:24 +0900

    로그 오류 수정(2)

--- config/settings/base.py
+++ config/settings/base.py
@@ -168,5 +168,9 @@ LOGGING = {
 			'level': 'INFO',
 			'propagate': False,
 		},
+		'pybo': {
+			'handlers': ['console', 'file'],
+			'level': 'INFO',
+		},
 	}
 }
--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -2,13 +2,15 @@ from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from ..models import Question
 from django.db.models import Q, Count
+import logging
+logger = logging.getLogger('pybo')
 
 def index(request):
 	"""
 	pybo 목록 출력
 	"""
+	logger.info("INFO 레벨로 출력")
 
-	3/0 # 강제로 오류 출력
 
 	# 입력 인자
 	page = request.GET.get('page', '1') # 페이지
